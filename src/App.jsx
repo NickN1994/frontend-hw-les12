f531341b85ed0711d0daf0bd8b37532452959e8a
NickN1994 <nickneggers@hotmail.com>
2023-11-29 12:06:54 +0100

    form bijna af

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,14 @@ function App() {
     const [banenenCounter, banenenSetCounter] = React.useState(0);
     const [appelCounter, appelSetCounter] = React.useState(0);
     const [kiwiCounter, kiwiSetCounter] = React.useState(0);
-    const [textField, setTextField] = React.useState("")
+    const [textField, setTextField] = React.useState("");
+    const [firstName, setFirstName] = React.useState("");
+    const [lastName, setLastName] = React.useState("");
+    const [age, setAge] = React.useState(0);
+    const [adress, setAdress] = React.useState("");
+    const [postcode, setPostcode] = React.useState("");
+    const [comment, setComment] = React.useState("");
+
 
 
     return (
@@ -56,51 +63,100 @@ function App() {
             </button>
 
             <div className="box-form">
-                <form action="">
+                <form onSubmit="Submit">
                     <fieldset>
+                        <div>
                         <p>Voornaam:</p>
                         <input type="text"
                                id="name"
                                name="name"
-                               value={textField}
-                               onChange={(e) => setTextField(e.target.value)}
+                               value={firstName}
+                               onChange={(e) => setFirstName(e.target.value)}
                         />
+                        </div>
+
+                        <div>
                         <p>Achternaam:</p>
                         <input type="text"
                                id="last-name"
                                name="last-name"
-                               value={textField}
-                               onChange={(e) => setTextField(e.target.value)}
+                               value={lastName}
+                               onChange={(e) => setLastName(e.target.value)}
                         />
+                        </div>
+
+                        <div>
                         <p>Leeftijd:</p>
                         <input type="number"
                                id="age"
                                name="age"
                                placeholder="0"
-                               value={textField}
-                               onChange={(e) => setTextField(e.target.value)}
+                               value={age}
+                               onChange={(e) => setAge(e.target.value)}
                         />
-                        <p>Adres</p>
+                        </div>
+
+                        <div>
+                        <p>Adres:</p>
                         <input type="text"
                                id="adress"
                                name="adress"
-                               value={textField}
-                               onChange={(e) => setTextField(e.target.value)}
+                               value={adress}
+                               onChange={(e) => setAdress(e.target.value)}
                         />
+                        </div>
+
+                        <div>
                         <p>Postcode</p>
                         <input type="text"
                                id="postcode"
                                name="postcode"
-                               value={textField}
-                               onChange={(e) => setTextField(e.target.value)}
+                               value={postcode}
+                               onChange={(e) => setPostcode(e.target.value)}
                         />
+                        </div>
+
+                        <div>
                         <p>Bezorgfrequentie:</p>
-                        <input type="text"
-                               id="age"
-                               name="age"
-                               value={textField}
-                               onChange={(e) => setTextField(e.target.value)}
-                        />
+                        <select name="bezorgfrequentie" id="bezorgfrequentie">
+                            <option value="Wekelijks">Wekelijks</option>
+                            <option value="om-de-2-weken">Om de 2 weken</option>
+                            <option value="maandelijks">Wekelijks</option>
+                        </select>
+                        </div>
+
+                        <div>
+                        <p>Bezorgdag:</p>
+                        <select name="bezorgdag" id="bezorgfrequentie">
+                            <option value="Maandag">Maandag</option>
+                            <option value="Dinsdag">Dinsdag</option>
+                            <option value="Woensdag">Woensdag</option>
+                            <option value="Donderdag">Donderdag</option>
+                            <option value="Vrijdag">Vrijdag</option>
+                        </select>
+                        </div>
+
+                        <div>
+                        <p>Bezorgmoment:</p>
+                        <input type="radio" id="overdag" name="overdag" value="Overdag" checked />
+                        <label htmlFor="Overdag">Overdag</label>
+                        <input type="radio" id="Avond" name="avond" value="Avond"/>
+                        <label htmlFor="Avond">Avond</label>
+                        </div>
+
+                        <div>
+                        <p>Opmerking</p>
+                            <textarea
+                                id="opmerking"
+                                name="opmerking"
+                                rows="4"
+                                cols="40"
+                                value={comment}
+                                onChange={(e) => setComment(e.target.value)}
+                            />
+
+                        </div>
+
                     </fieldset>
                 </form>
             </div>
